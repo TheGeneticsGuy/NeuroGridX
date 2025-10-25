d46ef60b74f68ff58ed3f65cb238e1160e5888c2
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-10-25 14:49:28 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+
+dotenv.config();
+
+connectDB(); // Connect to MongoDB
+
+const app: Express = express();
+const PORT = process.env.PORT || 5001;  // Local will be 5001
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// TEST ROUTE
+app.get('/api', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the NeuroGrid API!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
