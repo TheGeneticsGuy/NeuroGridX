fca05a6c5e18683599908fe9c468627ee4e65048
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-10-30 00:35:42 -0700

    Adjusted placeholder

--- client/src/pages/ChallengesPage.tsx
+++ client/src/pages/ChallengesPage.tsx
@@ -5,7 +5,7 @@ const ChallengesPage: React.FC = () => {
   return (
     <div>
       <h1>Challenges</h1>
-      <p>Select a challenge to play. Anyone can play!</p>
+      <p>Select a challenge. Anyone can play!</p>
       <div style={{ marginTop: '2rem' }}>
         <Link to="/challenges/click-accuracy" className="cta-button">
           Click Accuracy
